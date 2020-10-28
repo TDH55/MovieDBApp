5002a75c96eb615848182884cb2b9aa8426144dc
Taylor Howard <taylorhoward1@wustl.edu>
2020-10-27 19:36:42 -0500

    Initial Commit

---
+++ CSE439_Lab4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CSE439_Lab4
+//
+//  Created by Taylor Howard on 10/27/20.
+//  Copyright © 2020 Taylor Howard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
